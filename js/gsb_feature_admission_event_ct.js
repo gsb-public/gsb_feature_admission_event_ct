965cb4ae9754cf62f22f3b0e0200aeadfe0e1e40
Roopesh Naik <rnaik77@stanford.edu>
2015-06-26 23:43:06 +0530

    WP-5506 clear the time field in registration deadline

--- js/gsb_feature_admission_event_ct.js
+++ js/gsb_feature_admission_event_ct.js
@@ -1,7 +1,10 @@
 (function ($) {
   Drupal.behaviors.gsb_feature_admission_event_ct = {
     attach: function (context, settings) {
-      $('#edit-field-date-time-und-0-value-timeEntry-popup-1').val('');
+      var currentNid = Drupal.settings.gsb_feature_admission_event_ct.currentNid;
+      if (currentNid == null) {
+        $('#edit-field-date-time-und-0-value-timeEntry-popup-1').val('');
+      }
     }
   }
 })(jQuery);
