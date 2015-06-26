db83584bc029da8dd66d843941ad26ebddc345e2
Roopesh Naik <rnaik77@stanford.edu>
2015-06-26 23:05:57 +0530

    WP-5506 add new date field instance

---
+++ js/gsb_feature_admission_event_ct.js
@@ -0,0 +1,7 @@
+(function ($) {
+  Drupal.behaviors.gsb_feature_admission_event_ct = {
+    attach: function (context, settings) {
+      $('#edit-field-date-time-und-0-value-timeEntry-popup-1').val('');
+    }
+  }
+})(jQuery);
