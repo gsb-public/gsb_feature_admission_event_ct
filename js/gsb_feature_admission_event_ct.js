6b1d0211f424e0b6900106ca006a9bcced35bf37
Roopesh Naik <rnaik77@stanford.edu>
2015-07-03 17:47:30 +0530

    WP-5598 fix date fields

--- js/gsb_feature_admission_event_ct.js
+++ js/gsb_feature_admission_event_ct.js
@@ -1,11 +1,6 @@
 (function ($) {
   Drupal.behaviors.gsb_feature_admission_event_ct = {
     attach: function (context, settings) {
-      var currentNid = Drupal.settings.gsb_feature_admission_event_ct.currentNid;
-      if (currentNid == null) {
-        $('#edit-field-date-time-und-0-value-timeEntry-popup-1').val('');
-      }
-
       var cleanupFieldsets = function($event) {
         var hide_fieldgroups = {
           node_admission_event_form_group_description: "node_admission_event_form_group_description",
